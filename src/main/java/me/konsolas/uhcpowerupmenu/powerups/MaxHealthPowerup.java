e3271af83dba2e55d5058e16eab830984f8f46b5
FourNox <abhirup.mukherjee01@gmail.com>
2020-08-12 10:27:54 +0100

    Changes Changed cost of MaxHealthPowerup from 0 to 1 MaxHealthPowerup no longer sets player to half a heart MaxHealthPowerup now applies Wither I and Poison I for 5 seconds

--- src/main/java/me/konsolas/uhcpowerupmenu/powerups/MaxHealthPowerup.java
+++ src/main/java/me/konsolas/uhcpowerupmenu/powerups/MaxHealthPowerup.java
@@ -5,13 +5,15 @@ import org.bukkit.Material;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.attribute.AttributeInstance;
 import org.bukkit.entity.Player;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 import java.util.Objects;
 
 public class MaxHealthPowerup extends AbstractPowerup {
     public MaxHealthPowerup(UHCPowerupMenu plugin) {
-        super(plugin, Material.APPLE, true, -1, "Increase max health",
-                "Increases max hearts by 1 (up to a maximum of 10)", "Sets you to half a heart");
+        super(plugin, Material.APPLE, true, 1, "Increase max health",
+                "Increases max hearts by 1 (up to a maximum of 10)", "Poison I for 5 seconds", "Wither I for 5 seconds");
     }
 
     @Override
@@ -22,7 +24,8 @@ public class MaxHealthPowerup extends AbstractPowerup {
         } else {
             super.apply(target);
             maxHealthAttr.setBaseValue(maxHealthAttr.getBaseValue() + 2);
-            target.setHealth(1);
+            target.addPotionEffect(new PotionEffect(PotionEffectType.POISON, 100, 0));
+            target.addPotionEffect(new PotionEffect(PotionEffectType.WITHER, 100, 0));
         }
     }
 }
